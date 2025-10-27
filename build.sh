c105b09f3bf44e20855c72254def7129f79d0c37
Radu Stanciu <radu@kangu.ro>
2025-10-27 20:42:35 +0200

    build script

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
-GOOS=linux GOARCH=amd64 go build -o ./build/linux/cli-init
-echo "Linux build complete"
+GOOS=linux GOARCH=amd64 go build -o ./build/linux-x86/cli-init
+echo "Linux amd64 build complete"
+
+GOOS=linux GOARCH=arm64 go build -o ./build/linux-arm/cli-init
+echo "Linux arm64 build complete"
 
 # OSX
 go build -o ./build/osx/cli-init
